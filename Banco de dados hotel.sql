c98b1e3925f13f52534e87dce2586466372bb78b
Jones Dhyemison Quito de Oliveira <jones-dqo@CGE.local>
2018-01-02 09:29:10 -0200

    commit inicial do esquema de banco de dados

---
+++ Banco de dados hotel.sql
@@ -0,0 +1,17 @@
+﻿select usename from pg_user; --Lista os usuários de banco de dados
+
+select current_user -- Exibe o usuário atual
+
+select groname from pg_group; -- Lista os grupos de usuários(papeis) de banco de dados
+
+CREATE DATABASE hotel;
+
+-- CRIA A TABELA DE CLIENTES
+CREATE TABLE cliente
+(
+	rg numeric not null,
+	nome varchar(40) not null,
+	sexo char(1) not null,
+	telefone numeric(10,0),
+	primary key(rg)
+)without oids;
